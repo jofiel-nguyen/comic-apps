e5b9cecb30ac5998e9702d155976be04823f1454
Wren <thitunguyen@my.lonestar.edu>
2023-07-16 18:37:54 -0500

    update style for nav

--- Front-end/main.js
+++ Front-end/main.js
@@ -1,87 +1,4 @@
-// Search Functionality
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
 
-searchButton.addEventListener('click', performSearch);
-
-function performSearch() {
-  const searchTerm = searchInput.value;
-  console.log('Search term:', searchTerm);
-  function performSearch() {
-    const searchTerm = searchInput.value;
-    const comics = [
-      { title: 'Comic 1', author: 'John Doe' },
-      { title: 'Comic 2', author: 'Jane Smith' },
-      { title: 'Comic 3', author: 'Michael Johnson' },
-      // Add more comic objects as needed
-    ];
-  
-    // Filter the comics array based on the search term
-    const searchResults = comics.filter((comic) => {
-      const lowerCaseTitle = comic.title.toLowerCase();
-      const lowerCaseAuthor = comic.author.toLowerCase();
-      const lowerCaseSearchTerm = searchTerm.toLowerCase();
-      return (
-        lowerCaseTitle.includes(lowerCaseSearchTerm) ||
-        lowerCaseAuthor.includes(lowerCaseSearchTerm)
-      );
-    });
-  
-    // Clear previous search results
-    clearSearchResults();
-  
-    // Display search results in the UI
-    displaySearchResults(searchResults);
-  }
-  function clearSearchResults() {
-    const searchResultsContainer = document.getElementById('search-results');
-    searchResultsContainer.style.display = 'none'; // Hide the search results container
-  }
-  function displaySearchResults(results) {
-    const searchResultsContainer = document.getElementById('search-results');
-    
-    // Clear any previous search results
-    searchResultsContainer.innerHTML = '';
-  
-    // Iterate over the results array and create HTML elements for each search result
-    results.forEach((result) => {
-      // Create the necessary HTML elements
-      const resultItem = document.createElement('div');
-      resultItem.classList.add('search-result');
-      
-      const titleElement = document.createElement('h3');
-      titleElement.textContent = result.title;
-  
-      const authorElement = document.createElement('p');
-      authorElement.textContent = `Author: ${result.author}`;
-  
-      // Append the elements to the search results container
-      resultItem.appendChild(titleElement);
-      resultItem.appendChild(authorElement);
-      searchResultsContainer.appendChild(resultItem);
-    });
-  }
-  
-  
-}
-
-// Comic Card Click Event
-const comicCards = document.querySelectorAll('.comic-card');
-
-comicCards.forEach((card) => {
-  card.addEventListener('click', handleCardClick);
-});
-
-function handleCardClick(event) {
-  const clickedCard = event.currentTarget;
-  // Get the necessary information from the clicked card
-  const comicTitle = clickedCard.querySelector('h3').textContent;
-  const comicAuthor = clickedCard.querySelector('p').textContent;
-  // Perform actions based on the clicked card, such as displaying details or navigating to a new page
-  console.log('Clicked Comic:', comicTitle);
-  console.log('Author:', comicAuthor);
-  // Add your card click logic here
-}
 function openModal(pdfUrl) {
     var modal = document.getElementById("myModal");
     var modalPdf = document.getElementById("modalPdf");
@@ -97,4 +14,64 @@ function openModal(pdfUrl) {
     modal.style.display = "none";
     modalPdf.src = "";
   }
-  
+  // main.js
+
+// Function to handle the search button click event
+function handleSearch() {
+  var searchInput = document.getElementById("search-input").value;
+  var searchResults = document.getElementById("search-results");
+
+  // Clear previous search results
+  searchResults.innerHTML = "";
+
+  // Make API request to search for books/comics
+  var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + encodeURIComponent(searchInput);
+
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // Process the API response and display search results
+      if (data.totalItems > 0) {
+        for (var i = 0; i < data.items.length; i++) {
+          var item = data.items[i];
+          var title = item.volumeInfo.title;
+          var authors = item.volumeInfo.authors ? item.volumeInfo.authors.join(", ") : "Unknown Author";
+          var thumbnail = item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : "No Image";
+
+          // Create a card for each search result
+          var card = document.createElement("div");
+          card.classList.add("search-result-card");
+
+          var image = document.createElement("img");
+          image.src = thumbnail;
+          image.alt = title;
+
+          var titleElement = document.createElement("h3");
+          titleElement.textContent = title;
+
+          var authorElement = document.createElement("p");
+          authorElement.textContent = "Author: " + authors;
+
+          card.appendChild(image);
+          card.appendChild(titleElement);
+          card.appendChild(authorElement);
+
+          searchResults.appendChild(card);
+        }
+      } else {
+        // No search results found
+        var message = document.createElement("p");
+        message.textContent = "No results found.";
+        searchResults.appendChild(message);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+// Add event listener to the search button
+var searchButton = document.getElementById("search-button");
+searchButton.addEventListener("click", handleSearch);
