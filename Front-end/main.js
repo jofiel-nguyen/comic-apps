0e4043935581fedaa644b6ea54a01dc5eb3b4717
Wren <thitunguyen@my.lonestar.edu>
2023-07-16 14:46:05 -0500

    update main.js

--- Front-end/main.js
+++ Front-end/main.js
@@ -6,10 +6,63 @@ searchButton.addEventListener('click', performSearch);
 
 function performSearch() {
   const searchTerm = searchInput.value;
-  // Perform search operations based on the search term
-  // Update the UI with the search results
   console.log('Search term:', searchTerm);
-  // Add your search logic here
+  function performSearch() {
+    const searchTerm = searchInput.value;
+    const comics = [
+      { title: 'Comic 1', author: 'John Doe' },
+      { title: 'Comic 2', author: 'Jane Smith' },
+      { title: 'Comic 3', author: 'Michael Johnson' },
+      // Add more comic objects as needed
+    ];
+  
+    // Filter the comics array based on the search term
+    const searchResults = comics.filter((comic) => {
+      const lowerCaseTitle = comic.title.toLowerCase();
+      const lowerCaseAuthor = comic.author.toLowerCase();
+      const lowerCaseSearchTerm = searchTerm.toLowerCase();
+      return (
+        lowerCaseTitle.includes(lowerCaseSearchTerm) ||
+        lowerCaseAuthor.includes(lowerCaseSearchTerm)
+      );
+    });
+  
+    // Clear previous search results
+    clearSearchResults();
+  
+    // Display search results in the UI
+    displaySearchResults(searchResults);
+  }
+  function clearSearchResults() {
+    const searchResultsContainer = document.getElementById('search-results');
+    searchResultsContainer.style.display = 'none'; // Hide the search results container
+  }
+  function displaySearchResults(results) {
+    const searchResultsContainer = document.getElementById('search-results');
+    
+    // Clear any previous search results
+    searchResultsContainer.innerHTML = '';
+  
+    // Iterate over the results array and create HTML elements for each search result
+    results.forEach((result) => {
+      // Create the necessary HTML elements
+      const resultItem = document.createElement('div');
+      resultItem.classList.add('search-result');
+      
+      const titleElement = document.createElement('h3');
+      titleElement.textContent = result.title;
+  
+      const authorElement = document.createElement('p');
+      authorElement.textContent = `Author: ${result.author}`;
+  
+      // Append the elements to the search results container
+      resultItem.appendChild(titleElement);
+      resultItem.appendChild(authorElement);
+      searchResultsContainer.appendChild(resultItem);
+    });
+  }
+  
+  
 }
 
 // Comic Card Click Event
